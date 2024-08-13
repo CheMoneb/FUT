af2bedba3ed3e73fe3708f2dc68a04962191d2cc
Tarek Daoudi <tarek.daoudi@hotmail.fr>
2024-08-13 16:02:19 +0200

    index

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!(isset($_GET['page']))){
+   $page="erreur";
+}
+else {
+   $page=$_GET['page'];
+}
+echo $page;
+if ($page==="Players"){
+   echo "je suis dans la page Players!";
+   include __DIR__."/Controller/ControllerPlayers.php";
+   $controlleur=new ControllerPlayers();
+   $controlleur->findAll("Players");
+   include __DIR__."/Template/TemplatePlayers.php";
+
+}
+
+//if ($page==="Player"){
+   //echo "je suis dans la page Players!";
+   //include __DIR__."/Controller/ControllerPlayers.php";
+   //$controlleur=new ControllerPlayers();
+   //$controlleur->findBy("Players");
+   //include __DIR__."/Template/books.php";
+//}
