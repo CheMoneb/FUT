a3325f137e04902e77d0ead195b442a3a2253783
Tarek Daoudi <tarek.daoudi@hotmail.fr>
2024-08-14 14:11:43 +0200

    index fonctionnel tarek

--- Controller/ControllerPlayers.php
+++ Controller/ControllerPlayers.php
@@ -1,9 +1,13 @@
 <?php
 require_once __DIR__."/../Model/PlayersModel.php";
 require_once __DIR__."/Controller.php";
-$test ="test";
+
+
 class ControllerPlayers extends Controller {
+
+
     protected $model= "PlayersModel";
 
     
-}
+};
+?>
